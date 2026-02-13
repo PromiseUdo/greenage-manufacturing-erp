78b433037fe0f48e5ea70cf05f4ac8c88683eac7
Promise Udo <info.promiseudo@gmail.com>
2026-02-13 14:39:58 +0100

    update

--- app/(admin)/products/page.tsx
+++ app/(admin)/products/page.tsx
@@ -58,6 +58,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
+    cursor: "pointer",
   },
   "&:hover": {
     backgroundColor: theme.palette.action.selected,
